b853b2184ba9a0a763c646a32a596d55d4f34dd0
amrit <amrit.randhawa.ji@gmail.com>
2023-06-20 18:32:33 +0530

    push

--- lib/selectbrand.dart
+++ lib/selectbrand.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
@@ -7,10 +9,14 @@ class Brand extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
      backgroundColor: Colors.white24,
       body:  ListWheelScrollView(
         itemExtent: 200,
         children: [
+      CupertinoButton(child: const Text("Sign Out"), onPressed: (){
+        FirebaseAuth.instance.signOut();
+      }),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Container(
