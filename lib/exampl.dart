c77aaa4ae01854f6c9c43cc722f15d05e521cbde
amrit <amrit.randhawa.ji@gmail.com>
2023-06-20 18:24:53 +0530

    push

--- lib/exampl.dart
+++ lib/exampl.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:email_validator/email_validator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -23,49 +25,53 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                width: 250,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Form(
-                    key: emailKey,
-                    child: TextFormField(
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                        filled: true,
-                        hintText: "Email",
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("images/BLogin.jpg"), fit: BoxFit.cover)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 250,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Form(
+                  key: emailKey,
+                  child: TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
                         prefixIcon: const Icon(Icons.email_outlined,
-                            color: Colors.black54, size: 20),
+                            color: Colors.black),
                         fillColor: Colors.white,
+                        filled: true,
                         border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(25),
-                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(25.0),
                         ),
-                      ),
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return "Enter your email";
-                        } else if (!EmailValidator.validate(
-                            emailController.value.text)) {
-                          return "Email invalid";
-                        } else {
-                          return null;
-                        }
-                      },
-                      controller: emailController,
-                    ),
+                        hintText: "Email"),
+                    validator: (value) {
+                      if(value!.isEmpty){
+                        return  "Email required";
+                      }
+                      if (!EmailValidator.validate(emailController.value.text)) {
+                        return "Invalid Email";
+                      } else {
+                        return null;
+                      }
+                    },
+                    controller: emailController,
                   ),
                 ),
               ),
-              SizedBox(
-                width: 250,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
+            ),
+            SizedBox(
+              width: 250,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Form(
+                  key: passwordKey,
                   child: TextFormField(
                     obscureText: _isVisible ? false : true,
                     controller: passwordController,
@@ -84,9 +90,13 @@ class _LoginState extends State<Login> {
                     ),
 
                     validator: (e) {
-                      if (emailController.value.text.isEmpty) {
+                      if (passwordController.value.text.isEmpty) {
                         return "Enter Password";
-                      } else {
+                      }
+                      else if(passwordController.value.text.length < 8){
+                        return "8 characters required";
+                      }
+                      else {
                         return null;
                       }
                     },
@@ -94,45 +104,47 @@ class _LoginState extends State<Login> {
                   ),
                 ),
               ),
-              ElevatedButton(
-                  child:
-                      const Text("Login", style: TextStyle(color: Colors.white)),
-                  onPressed: () {
-                    setState(() {});
-                    if (emailKey.currentState!.validate()) {}
-                  }),
-              ElevatedButton(
-                  child: const Text("Create a account",
-                      style: TextStyle(color: Colors.white)),
-                  onPressed: () {
-                    signup();
-                  }),
-              // ElevatedButton(
-              //     child: const Text("Check Demo",
-              //         style: TextStyle(color: Colors.white)),
-              //     onPressed: () {
-              //       // if (emailKey.currentState!.validate()) {
-              //       //   login();
-              //       // }
-              //     }),
-              AnimatedButton(
-                width: 150,
-                backgroundColor: Colors.blue,
-                text: 'Login With OTP',
-                selectedTextColor: Colors.black,
-                transitionType: TransitionType.BOTTOM_TO_TOP,
-                textStyle: const TextStyle(
-                  color: Colors.white,
-                ),
-                onPress: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const LoginOtp()),
-                  );
-                },
-              )
-            ],
-          ),
+            ),
+            ElevatedButton(
+                child:
+                    const Text("Login", style: TextStyle(color: Colors.white)),
+                onPressed: () {
+
+                  if (emailKey.currentState!.validate()& passwordKey.currentState!.validate()) {
+                    login(emailController.value.text,passwordController.value.text);
+                  }
+                }),
+            ElevatedButton(
+                child: const Text("Create a account",
+                    style: TextStyle(color: Colors.white)),
+                onPressed: () {
+                  signup();
+                }),
+            // ElevatedButton(
+            //     child: const Text("Check Demo",
+            //         style: TextStyle(color: Colors.white)),
+            //     onPressed: () {
+            //       // if (emailKey.currentState!.validate()) {
+            //       //   login();
+            //       // }
+            //     }),
+            AnimatedButton(
+              width: 150,
+              backgroundColor: Colors.blue,
+              text: 'Login With OTP',
+              selectedTextColor: Colors.black,
+              transitionType: TransitionType.BOTTOM_TO_TOP,
+              textStyle: const TextStyle(
+                color: Colors.white,
+              ),
+              onPress: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LoginOtp()),
+                );
+              },
+            )
+          ],
         ),
       ),
     );
@@ -146,14 +158,27 @@ class _LoginState extends State<Login> {
     } on FirebaseAuthException catch (e) {
       if (e.code == 'weak-password') {
       } else if (e.code == 'email-already-in-use') {}
-    } catch (e) {}
+    }
   }
+
+  Future<void> login(String email, String password) async {
+    try{
+      FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password).then((value) => {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => const Brand(),))
+      });
+  } on FirebaseAuthException catch (e) {
+    if(e.code == "wrong-password"){
+      print("wrong password");
+    }
+  }
+
 
   void updateStatus() {
     setState(() {
-      if (_isVisible) {
+      if(_isVisible){
         _isVisible = false;
-      } else {
+      }
+      else{
         _isVisible = true;
       }
     });
